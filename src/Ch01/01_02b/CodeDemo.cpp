20753885f6a132ce2ebc9f9f7c6da05664e6cc97
ndalba <74461294+ndalba@users.noreply.github.com>
2026-06-12 13:46:14 +0000

    Added main function code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,13 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi there!" << std::endl;
+
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}

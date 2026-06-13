797e607a4ea29bbe3bfa2e96b5af682c683fbc44
ndalba <74461294+ndalba@users.noreply.github.com>
2026-06-13 13:24:18 +0000

    Added code

--- src/Ch03/03_08/CodeDemo.cpp
+++ src/Ch03/03_08/CodeDemo.cpp
@@ -9,6 +9,8 @@ double my_average(int a, int8_t b, uint32_t c){
     double result = 0.0;
 
     // Write your code here
+
+    result = static_cast<double> (a + b + c)/3.0;
     
     return result;
 }
